648b979e8c04a9a3f4bd4b0d777ed975b927a92c
HarryKentGames <harrygckent@gmail.com>
2020-08-28 11:38:14 +0100

    Added node graph creation

---
+++ Pathfinding/Source/Pathfinding/GraphNodeNetwork.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "DrawDebugHelpers.h"
+#include "GraphNode.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
+#include "GraphNodeNetwork.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PATHFINDING_API UGraphNodeNetwork : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGraphNodeNetwork();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	TArray<UGraphNode*> CreateNetwork();
+	TArray<UGraphNode*> GetNodes();
+	float GetResolution();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float resolution;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<UGraphNode*> nodes;
+
+	FVector bottomLeft;
+	FVector topRight;
+	float roomWidth;
+	float roomLength;
+
+	void GetRoomSize();
+	void GenerateNetwork(const UNavigationSystemV1* navSys);
+	void CreateMovementNetwork(const UNavigationSystemV1* navSys);
+	void DebugDraw();
+};
